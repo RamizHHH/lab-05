84b2722dc7600e537041171c18d5e2aa8eb829c9
RamizHHH <RamizHussain500@Gmail.com>
2025-10-07 18:20:22 -0600

    Finished

--- ListyCity/app/src/main/java/com/example/lab5_starter/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/lab5_starter/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.lab5_starter;
 
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.executor.ArchTaskExecutor;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 
@@ -20,6 +33,8 @@ public class MainActivity extends AppCompatActivity implements CityDialogFragmen
 
     private ArrayList<City> cityArrayList;
     private ArrayAdapter<City> cityArrayAdapter;
+
+    private CollectionReference citiesRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +56,6 @@ public class MainActivity extends AppCompatActivity implements CityDialogFragmen
         cityArrayAdapter = new CityArrayAdapter(this, cityArrayList);
         cityListView.setAdapter(cityArrayAdapter);
 
-        addDummyData();
 
         // set listeners
         addCityButton.setOnClickListener(view -> {
@@ -54,6 +68,37 @@ public class MainActivity extends AppCompatActivity implements CityDialogFragmen
             CityDialogFragment cityDialogFragment = CityDialogFragment.newInstance(city);
             cityDialogFragment.show(getSupportFragmentManager(),"City Details");
         });
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        citiesRef = db.collection("cities");
+
+        citiesRef.addSnapshotListener((value, error) -> {
+           if (error != null){
+               Log.e("Firestore", error.toString());
+           }
+           if(value != null && !value.isEmpty()){
+               cityArrayList.clear();
+               for(QueryDocumentSnapshot snapshot : value){
+                   String name = snapshot.getString("name");
+                   String province = snapshot.getString("province");
+
+                   cityArrayList.add(new City(name, province));
+               }
+               cityArrayAdapter.notifyDataSetChanged();
+           }
+        });
+
+        cityListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                City city = cityArrayAdapter.getItem(position);
+                DocumentReference docRef = citiesRef.document(city.getName());
+                docRef.delete();
+                cityArrayList.remove(city);
+                cityArrayAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
 
     }
 
@@ -70,6 +115,8 @@ public class MainActivity extends AppCompatActivity implements CityDialogFragmen
     public void addCity(City city){
         cityArrayList.add(city);
         cityArrayAdapter.notifyDataSetChanged();
+        DocumentReference docRef = citiesRef.document(city.getName());
+        docRef.set(city);
 
     }
 
